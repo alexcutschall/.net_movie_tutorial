39ae388fb6b5895172675c055744b8c9004056d7
alexcutschall <alexander.cutschall@gmail.com>
2021-09-07 10:53:50 -0600

    Adding in a README and a new controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,9 +14,10 @@ namespace MvcMovie.Controllers
             return "This is my default action...";
         }
 
-        public string Welcome()
+        public string Welcome(String name, int numTimes = 1)
         {
-            return "This is my Welcome action...";
+            return HtmlEncoder.Default.Encode($"Welcome, {name}, NumTimes is {numTimes}");
+
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+Working through the tutorial https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-5.0&tabs=visual-studio
